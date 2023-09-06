804406366398dfc4a542080a535a88bb238e6ff5
muhammadhashirsafdar <139388035+muhammadhashirsafdar@users.noreply.github.com>
2023-09-07 00:34:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,183 @@
+// !       MATH EXPRESSION     ! \\
+
+
+// ? Question # 1
+
+// var num = 3;
+// var num2 = 4;
+// var result = 3 + 4;
+// document.write("sum of 3 and 4 is = " , result);
+
+
+// ?  Question # 2
+
+
+// var num = 3;
+// var num2 = 4;
+// var result = 3 * 4;
+// document.write("Multiplication of 3 and 4 is = " , result);
+
+
+//   * for subtraction
+
+
+// var num = 3;
+// var num2 = 4;
+// var result = 3 - 4;
+// document.write("subtraction of 3 and 4 is = " , result);
+
+
+// * for division 
+
+
+// var num = 3;
+// var num2 = 4;
+// var result = 4 / 2;
+// document.write("Division of 2 and 4 is = " , result);
+
+
+// * for Modulous
+
+
+// var num = 3;
+// var num2 = 4;
+// var result = 3 % 4;
+// document.write("Modulous of 3 and 4 is = " , result);
+
+
+
+// ? Question # 3   
+
+
+// var ab = "value after variable after decleration is 5";
+// var cd = "initial value: ";
+// var ef = "value afte increment is: ";
+// var gh = "value after adding 7 is: ";
+// var ij = "value after decrement is: ";
+// var kl = "The remainder is: 0";
+
+// var num1 = 5;
+// var num2 = 1;
+// var result = num1 + num2;
+// var num3 = 7;
+// var result2 = result + num3;
+// var num4 = 1;
+// var result3 = result2 - num4;
+
+// document.write(ab);
+// document.write("<br>");
+// document.write(cd,num1);
+// document.write("<br>");
+// document.write(ef,result);
+// document.write("<br>");
+// document.write(gh,result2);
+// document.write("<br>");
+// document.write(ij,result3);
+// document.write("<br>");
+// document.write(kl);
+
+
+
+// ?  Question # 4
+
+// var num1 = 450;
+// var num2 = 5;
+// var result = num1*num2;
+
+// document.write("Total cost to buy 5 tickets for a movie is: ", result);
+
+
+// ? Quetion # 5
+
+
+// ? Question 6
+
+// var ab = "celsius to farenheit";
+// document.write(ab);
+// document.write("<br>");
+
+// var C = 50;
+// var result = (C*(9/5))+32;
+// document.write("Tempratuer in Celsius = ",C);
+// document.write("<br>");
+// document.write("50 in Celsius is", result ,"in Farenheit");
+
+
+
+// var ab = "farenheit to celsius";
+// document.write(ab);
+// document.write("<br>");
+
+// var F = 20;
+// var result = (F-32)/1.8;
+
+// document.write("Tempratuer in Farenheit = ",F);
+// document.write("<br>");
+// document.write("20 in Farenheit is", result ,"in celsius");
+
+
+
+// ? Question 7 
+
+// var item1 = 950;
+// var item1quantity = 3;
+// var item2 = 500;
+// var item2quantity = 7;
+// var shippingcharges = 100;
+
+// var result = item1 * item1quantity;
+// var result2 = item2 * item2quantity;
+// var result3 = result + result2 + shippingcharges;
+
+// document.write("Price of item 1 = ", item1);
+// document.write("<br>");
+// document.write("quantity of item1 = ", item1quantity);
+// document.write("<br>");
+// document.write("Price of ite2 = ", item2);
+// document.write("<br>");
+// document.write("Quantity of item2 = ", item2quantity);
+// document.write("<br>");
+// document.write("Shipping Charges = ",shippingcharges);
+// document.write("<br>");
+// document.write("<br>");
+// document.write("Total cost of your Order = ",result3);
+
+
+// ?  Question # 8
+
+
+// var num = 941;
+// var num2 = 1100;
+// var result = num / num2 * 100;
+
+// document.write("Total Marks = ",num2);
+// document.write("<br>");
+// document.write("Obtained Marks = ", num);
+// document.write("<br>");
+// document.write("Percentage = ",result);
+
+
+// ? Question # 9
+
+// var usdollar = 10;
+// var pkr = 308;
+
+// var result = usdollar*pkr
+
+// document.write("Total currency in PKR = ",result)
+
+
+// ? Question 10
+
+
+
+// var num = 15;
+// var result = num + 5 * 10 / 2;
+
+// document.write(num);
+// document.write("<br>");
+// document.write(result);
+
+// ? Question # 11
+// ? Question # 12
+// ? Question # 13
